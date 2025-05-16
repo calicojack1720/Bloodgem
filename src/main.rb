130d93af6c4f0b1d42239c99817f6d37238f4e01
calicojack1720 <mtastfalkjr@verizon.net>
2025-05-16 12:26:00 -0400

    Began option menu

---
+++ src/main.rb
@@ -0,0 +1,13 @@
+# main.rb
+# calicojack1720
+# Created: 5/14/25
+# Updated: 5/16/25
+
+option = -1
+
+while option != 0 do
+  puts "Enter an option:"
+  puts "0 - Quit"
+
+  option = gets.chomp.to_i
+end
